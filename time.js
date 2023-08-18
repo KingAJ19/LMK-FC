284569e3e34a6776cf9ff13b388afa6bc4096494
KingAJ <65874068+KingAJ19@users.noreply.github.com>
2023-08-18 11:08:49 +0200

    changes

--- time.js
+++ time.js
@@ -1,3 +1,5 @@
+// FISRT ONE
+
 // var today = new Date();
 // var day = today.getDay();
 // var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
@@ -6,10 +8,11 @@
 // var dateTime = date+' '+time;
 // (add after dateTime) + ' <br>- ' + daylist[day];
 
+// SECOND ONE
+
 var today = new Date();
 var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
 var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 var dateTime = date+' '+time;
- 
 document.getElementById("time").innerHTML = dateTime 
 
