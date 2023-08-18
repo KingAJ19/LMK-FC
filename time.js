ac394a17c5b11b399bb254c037fa821e9dd882f2
KingAJ <65874068+KingAJ19@users.noreply.github.com>
2023-08-18 10:51:35 +0200

    new format

--- time.js
+++ time.js
@@ -1,3 +1,15 @@
-var datetime = new Date();
-console.log(datetime);
-document.getElementById("time").textContent = datetime;
+// var today = new Date();
+// var day = today.getDay();
+// var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+// var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+// var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+// var dateTime = date+' '+time;
+// (add after dateTime) + ' <br>- ' + daylist[day];
+
+var today = new Date();
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var dateTime = date+' '+time;
+ 
+document.getElementById("time").innerHTML = dateTime 
+
