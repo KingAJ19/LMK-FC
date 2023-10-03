b5d634c65ca19c14fa809c6a35df466211d42bb7
KingAJ <65874068+KingAJ19@users.noreply.github.com>
2023-10-03 20:29:34 +0200

    rynell

--- js/modal.js
+++ js/modal.js
@@ -190,6 +190,27 @@ span.onclick = function() {
   modal.style.display = "none";
 }
 
+// Rynell
+var modal = document.getElementById("myModal9");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg9");
+var modalImg = document.getElementById("img10");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close9")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+
 
 
 
